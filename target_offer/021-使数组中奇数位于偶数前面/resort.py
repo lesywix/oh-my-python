711db96c200f6b50c654f31cdcc93f910a93ca6f
freeflowthought <73615614+freeflowthought@users.noreply.github.com>
2023-01-09 08:24:57 -0500

    Update resort.py
    refactoring the previous commit

--- target_offer/021-使数组中奇数位于偶数前面/resort.py
+++ target_offer/021-使数组中奇数位于偶数前面/resort.py
@@ -1,37 +1,42 @@
-"""
-提目：输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
-总结：合理运用 Python 迭代器特性解决问题
-"""
+'''
+题目：
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+'''
 import unittest
-
-#fix the bug in your code
 
 class Solution:
     """
     @param: nums: an array of integers
-    @return: nothing
+            func: the filtering function
     """
-    def reorder_odd_even(self, nums):
-        if not isinstance(nums, list) or len(nums) == 0:
+    def reorder_odd_even(self, nums, func):
+        if not isinstance(nums, list):
+            raise TypeError("Invalid input type")
+        if len(nums) <= 0:
             return
         begin = 0
         end = len(nums) - 1
         while begin < end:
-            while begin < end and (nums[begin] & 0x1) ==1:
+            while begin < end and not func(nums[begin]):
                 begin += 1
-            while begin < end and (nums[end] & 0x1) == 0:
+            while begin < end and func(nums[end]):
                 end -= 1
             if begin < end:
                 nums[begin], nums[end] = nums[end], nums[begin]
                 begin += 1
         return nums
 
+def is_even(n):
+    return n & 1 == 0
+
 s = Solution()
-print(s.reorder_odd_even([1,2,3,4,5]))
+#print(s.reorder_odd_even([2,1],is_even))
+#print(s.reorder_odd_even((2,1),is_even))
+#print(s.reorder_odd_even([],is_even))
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self.assertEqual([1, 3, 2, 4],Solution().reorder_odd_even([1,2,3,4]))
+        self.assertEqual([1, 3, 2, 4],Solution().reorder_odd_even([1,2,3,4],is_even))
 
-Test().test()
+#Test().test()
